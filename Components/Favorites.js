4f95080f0ec2f492dab6973ce65419bf4502434e
Chad M <chadmusick@gmail.com>
2022-08-28 09:07:03 -0400

    favorite list works now and appears to send correct information to recipe display screen

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -21,8 +21,8 @@ export function Favorites({ route, navigation }) {
 
     const user = auth().currentUser;
 
-    function selectRecipe(item) {
-        navigation.navigate("Display Recipe", { loadedID: item[0], loadedRecipe: item[1] });
+    function selectRecipe(item, key, value) {
+        navigation.navigate("Display Recipe", { loadedID: key, loadedRecipe: value });
     }
 
     function getData() {
@@ -32,15 +32,11 @@ export function Favorites({ route, navigation }) {
             .then((snapshot) => {
                 if (snapshot.exists()) {
                     let favArray = []
-                    snapshot.forEach((childSnap) => {
-                        // setLoadedData(Object.entries(childSnap.val()))
-                        childSnap.forEach((item)=> {
+                    snapshot.forEach((item) => {
                             favArray.push(item.val())
-                        })
-                        // console.log([...favArray])
-                        setLoadedData(favArray);
-                        
                     })
+                    setLoadedData(favArray);
+
                 } else {
                     console.log("No data available");
                 }
@@ -65,18 +61,17 @@ export function Favorites({ route, navigation }) {
 
     const displayData = 
 
-// console.log(loadedData[0]["Recipe Name"])
         
-            loadedData
-                .filter((item) => item.favorite === true)
-                .map((item, index) =>
+            Object.values(loadedData)
+                .map((item, index) => Object.entries(item).filter(([key, value]) => value.favorite ==true )
+                .map(([key, value]) =>
                 (
-                    <TouchableOpacity style={styles.entry} key={index} onPress={() => selectRecipe(item)}>
-                        <Text style={styles.entryHeadline}>{item["Recipe Name"]}</Text>
-                        <Text style={styles.entryMethod}>{item.method}</Text>
-                        <Text style={styles.entryDesc}>{item["Description"]}</Text>
-                        {item.favorite == true ? <Image source={favoriteSmiley} style={styles.favorite} /> : <Text> </Text>}
-                    </TouchableOpacity>))
+                    <TouchableOpacity style={styles.entry} key={index} onPress={() => selectRecipe(item, key, value)}>
+                        <Text style={styles.entryHeadline}>{value["Recipe Name"].variableValue}</Text>
+                        <Text style={styles.entryMethod}>{value.method}</Text>
+                        <Text style={styles.entryDesc}>{value["Description"].variableValue}</Text>
+                        {value.favorite == true ? <Image source={favoriteSmiley} style={styles.favorite} /> : <Text> </Text>}
+                    </TouchableOpacity>)))
     
 
 
